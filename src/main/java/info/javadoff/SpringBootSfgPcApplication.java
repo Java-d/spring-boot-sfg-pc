db11f9b32634c55f4069b26c5c47a9fadf7cd5f5
Emin Javadov <java-d@rambler.ru>
2018-09-21 15:46:04 +0400

    Project initialization

---
+++ src/main/java/info/javadoff/SpringBootSfgPcApplication.java
@@ -0,0 +1,12 @@
+package info.javadoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSfgPcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSfgPcApplication.class, args);
+    }
+}
